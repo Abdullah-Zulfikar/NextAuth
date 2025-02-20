ef32ad08887c19193bc1ca2b967ee2b1e19c648d
Abdullah-Zulfikar <B22F0275AI131@fecid.paf-iast.edu.pk>
2025-02-20 22:17:06 +0500

    Refactor Home component to enhance user experience with conditional rendering based on authentication status; update sign-in button functionality.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
 "use client"
 
+import { signIn, useSession } from "next-auth/react";
+
 
 
 export default function Home() {
+  const { data: session} = useSession()
   return (
-    <div>
-      {" "}
-      <p>You are not signed in </p> <button> Sign in with github</button>
-    </div>
+    <>
+    { session ? (
+      <>
+        <h1>Welcome Back</h1>
+        { JSON.stringify(session)}
+        {session.user?.email}
+      </>
+
+    ) : (
+      <>
+        <h1>You are not logged in </h1>
+        <button onClick={()=>signIn("github")}>Sign in with github</button>
+      </>
+    )
+
+    }
+    </>
   );
 }
