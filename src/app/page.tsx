c8eacbc81a086ad14de56a635db704dd6bb410cb
Abdullah-Zulfikar <B22F0275AI131@fecid.paf-iast.edu.pk>
2025-03-04 10:12:40 +0500

    Add Google sign-in button to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
       <>
         <h1>You are not logged in </h1>
         <button onClick={()=>signIn("github")}>Sign in with github</button>
+        <button onClick={()=>signIn("google")}>Sign in with google</button>
       </>
     )
 
