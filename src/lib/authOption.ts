e896be90975ddc89d91852c31774b870d8c4a453
Abdullah-Zulfikar <B22F0275AI131@fecid.paf-iast.edu.pk>
2025-03-04 09:50:59 +0500

    Refactor Home component to enhance user experience with conditional rendering based on authentication status; update sign-in button functionality.

--- src/lib/authOption.ts
+++ src/lib/authOption.ts
@@ -1,6 +1,7 @@
 
 import { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google"
 
 // Define NextAuth configuration
 export const authOptions: NextAuthOptions = {
@@ -9,6 +10,10 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.AUTH_GITHUB_ID || "",
       clientSecret: process.env.AUTH_GITHUB_SECRET || ""
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+    })
     
   ]
 }; 
